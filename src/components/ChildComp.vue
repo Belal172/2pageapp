13b691768aad43fe4ff2ace4064cc53fc5811c5d
Milan Belal <mohitbelal9@gmail.com>
2025-03-28 14:48:19 +0545

    handle child coponent event from parent component

---
+++ src/components/ChildComp.vue
@@ -0,0 +1,14 @@
+<template>
+<button @click="sendMessage">Click me</button>
+</template>
+
+<script>
+export default{
+    name:'ChildComp',
+    methods:{
+       sendMessage(){
+        this.$emit("ChildEvent","Hello from child");
+       }
+    },
+}
+</script>
