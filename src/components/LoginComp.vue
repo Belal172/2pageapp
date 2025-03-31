35f64e1a7b232b116b71a2820d76a1e5c344149f
Milan Belal <mohitbelal9@gmail.com>
2025-03-31 17:10:11 +0545

    login form corrected with new field password

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -1,41 +1,44 @@
 <template>
-<div class="login-form">
-    <h1>login form</h1>
-    <Form action="" @submit="onsubmit">
-        <label for="email">Email:<Field type="email" name="email" id="email" :rules="validateEmail" /></label><br>
-        <ErrorMessage name="email" />
-        <button>Signup</button>
-
-    </form>
-</div>
-</template>
-
-<script>
-import {
-    Field,
-    Form,
-    ErrorMessage
-} from 'vee-validate';
-export default {
+    <div class="login-form">
+      <h1>Login Form</h1>
+      <Form @submit="onsubmit">
+        <label for="email">Email:</label>
+        <Field name="email" type="email" id="email" :rules="validateEmail" /><br>
+        <ErrorMessage name="email" style="color: red;" /><br>
+  
+        <label for="password">Password:</label>
+        <Field name="password" type="password" id="password" :rules="validatePassword" /><br>
+        <ErrorMessage name="password" style="color: red;" /><br>
+  
+        <button type="submit">Signup</button>
+      </Form>
+    </div>
+  </template>
+  
+  <script>
+  import { Field, Form, ErrorMessage } from 'vee-validate';
+  
+  export default {
     name: 'LoginComp',
     components: {
-        Field,
-        Form,
-        ErrorMessage
+      Field,
+      Form,
+      ErrorMessage
     },
     methods: {
-        onsubmit(values) {
-            console.warn(values)
-
-        },
-        validateEmail(values) {
-            if (!values) {
-                return 'this field is required';
-            }
-            return true;
-        }
-       
-    },
-
-}
-</script>
+      onsubmit(values) {
+        console.warn("Form Submitted:", values);
+      },
+      validateEmail(value) {
+        if (!value) return 'Email field is required';
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(value) ? true : 'Please enter a valid email address';
+      },
+      validatePassword(value) {
+        if (!value) return 'Password field is required';
+        return value.length >= 6 ? true : 'Password must be at least 6 characters long';
+      }
+    }
+  }
+  </script>
+  
